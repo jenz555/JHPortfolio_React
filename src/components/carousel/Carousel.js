f88a70d023eb7d21cd682c66a2e296b0dc92e408
Hiar, Jennifer <Jennifer.Hiar@allstate.com>
2017-02-10 07:58:32 -0600

    reverting back

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,6 +1,6 @@
 import React from 'react'
 var Slider = require('react-slick');
-import { Link } from 'react-router'
+import {Link} from 'react-router'
 
 import Bloom from '../bloom/Bloom'
 import Provena from '../provena/Provena'
@@ -10,96 +10,81 @@ import WalkInWoods from '../walkInWoods/WalkInWoods'
 import RoadSmart from '../roadSmart/RoadSmart'
 import Footer from '../footer/Footer'
 
-
 var Carousel = React.createClass({
-    render: function () {
-        var settings = {
-            dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
+  render: function () {
+    var settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false
+    };
 
-        };
+    return (
+      <Slider {...settings}>
 
-        return (
-        <Slider {...settings}>
-
-            <div>
-           <div className="wrapper">
-             <div className="flexbox__container">
-                <div className="flexbox__item">
+        <div>
+          <div className="wrapper">
+            <div className="flexbox__container">
+              <div className="flexbox__item">
                 <div className="flexbox__header flexbox__header--design">
-                 <img src="../images/designHeader.svg" className="img img--fill"/>
+                  <img src="../images/designHeader.svg" className="img img--fill"/>
                 </div>
-                </div>
-             </div>
-             </div>
-              <div className="flexbox__container">
-                <div className="flexbox__item flexbox__item--bib">
-                <Link to="/BloomDetails">
+              </div>
+            </div>
+          </div>
+          <div className="flexbox__container">
+            <div className="flexbox__item flexbox__item--bib">
+              <Link to="/BloomDetails">
                 <Bloom></Bloom>
-                </Link>
-               
-                
-                </div>
-                <div className="flexbox__item flexbox__item--provena">
-                <WalkInWoods></WalkInWoods>
+              </Link>
+
+            </div>
+            <div className="flexbox__item flexbox__item--provena">
+              <WalkInWoods></WalkInWoods>
+            </div>
+          </div>
+
+          <div className="flexbox__container">
+            <div className="flexbox__item flexbox__item--bib">
+              <Logos></Logos>
+            </div>
+            <div className="flexbox__item flexbox__item--provena">
+              <Provena></Provena>
+            </div>
+          </div>
+
+        </div>
+
+        <div>
+          <div className="wrapper">
+
+            <div className="flexbox__container">
+              <div className="flexbox__item">
+                <div className="flexbox__header flexbox__header--develop">
+                  <img src="../images/developHeader.svg" className="img img--fill"/>
                 </div>
               </div>
 
-               <div className="flexbox__container">
-                <div className="flexbox__item flexbox__item--bib">
-                <Logos></Logos>
-                </div>
-                <div className="flexbox__item flexbox__item--provena">
-                <Provena></Provena>
-                </div>
+            </div>
+
+            <div className="flexbox__container">
+              <div className="flexbox__item">
+                <img src="../images/roadSmart.png" className="img img--fill"/>
               </div>
 
-               
-            </div>
-            
-            <div>
-            <div className="wrapper">
-
-             <div className="flexbox__container">
-                <div className="flexbox__item">
-                  <div className="flexbox__header flexbox__header--develop">
-                   <img src="../images/developHeader.svg" className="img img--fill"/>
-                  </div>
-                </div>
-
-             </div>
-
-             <div className="flexbox__container">
-                <div className="flexbox__item">
-                   <img src="../images/roadSmart.png" className="img img--fill"/>
-                </div>
-
-                 <div className="flexbox__item">
-                  <RoadSmart></RoadSmart>
-                </div>
-                </div>
-
-
+              <div className="flexbox__item">
+                <RoadSmart></RoadSmart>
+              </div>
             </div>
 
-            </div>
-
-            
-
-        
-
+          </div>
+        </div>
       </Slider>
 
-
-
-
-
-        );
-    }
+    );
+  }
 });
 
 module.exports = Carousel;
